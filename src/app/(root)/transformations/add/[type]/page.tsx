5be53b08d053fa51e229c5ecfa1d6cbb4efe4d65
FAHIM <azmirhasan.az2@gmail.com>
2024-05-13 20:18:32 +0600

    add new components

--- src/app/(root)/transformations/add/[type]/page.tsx
+++ src/app/(root)/transformations/add/[type]/page.tsx
@@ -1,11 +1,32 @@
-import React from 'react'
+import Header from '@/components/shared/Header';
+import TransformationForm from '@/components/shared/TransformationForm';
+import { transformationTypes } from '@/constants/constants';
+import { getUserById } from '@/lib/action/user.action';
+import { auth } from '@clerk/nextjs/server';
+import { redirect } from 'next/navigation';
 
-type Props = {}
+const Page = async ({ params: { type } }: SearchParamProps) => {
+  const { userId } = auth();
 
-const page = (props: Props) => {
+  if (!userId) redirect('/sign-in');
+  const user = await getUserById(userId);
+
+  const transformation = transformationTypes[type];
+
   return (
-    <div>page</div>
-  )
-}
+    <>
+      <Header title={transformation.title} subtitle={transformation.subTitle} />
 
-export default page
+      <section className='mt-10'>
+        <TransformationForm
+          action='Add'
+          userId={user._id}
+          type={transformation.type as TransformationTypeKey}
+          creditBalance={user.creditBalance}
+        />
+      </section>
+    </>
+  );
+};
+
+export default Page;
